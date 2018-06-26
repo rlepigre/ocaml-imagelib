ec92a42ea8d649487b56d1e418144de6e60df039
Dan Ravensloft <dan.ravensloft@gmail.com>
2018-06-26 01:34:32 +0100

    Rewrite JPEG size code

--- imageJPG.ml
+++ imageJPG.ml
@@ -23,64 +23,41 @@ open Image
 module ReadJPG : ReadImage = struct
   let extensions = ["jpg"; "jpeg"; "jpe"; "jif"; "jfif"; "jfi"]
 
-  let read_marker ich =
-    let ff = chunk_byte ich in
-    if ff <> 0xff then
-      raise (Corrupted_image "Expected marker...");
-    let rec read_first_not_ff ich =
-      let c = chunk_byte ich in
-      if c = 0xff then read_first_not_ff ich else c
-    in
-    let c = read_first_not_ff ich in
-    if c = 0 then
-      raise (Corrupted_image "0xFF00 is not a valid marker...");
-    c
+  let int_of_str1 s =
+    Char.code s.[0]
 
-  let read_header_data ich =
-    (* Read magic number (i.e. marker 0xffd8) *)
-    let magic = read_marker ich in
-    if magic <> 0xd8 then
-      raise (Corrupted_image "First marker should be SOI...");
+  let int_of_str2 s =
+    (Char.code s.[1]) + ((Char.code s.[0]) lsl 8)
 
-    (* Read other header sections *)
-    let rec read_header_sections ich acc =
-      let mrk = read_marker ich in
-      let sz = get_bytes ich 2 in
-      let size = ((int_of_char sz.[0]) lsl 8) lor (int_of_char sz.[1]) in
-      let data = get_bytes ich (size - 2) in
-      if mrk = 0xda then (* SOS reached *)
-        List.rev ((mrk, data) :: acc)
-      else
-        read_header_sections ich ((mrk, data) :: acc)
-    in
-    read_header_sections ich []
-
-  (* Read the size of a XCF image.
-   * Arguments:
-   *   - fn : filename.
-   * Returns a couble (width, height).
-   * Note: the image is not checked for inconsistency, only the signature and
-   * header are checked.
-   *)
+  (* Based on https://stackoverflow.com/a/48488655 *)
   let size ich =
-    let headers = read_header_data ich in
-    close_chunk_reader ich;
-
-    let rec find_SOF ls =
-      match ls with
-      | []        -> raise (Corrupted_image "No SOFn marker...");
-      | (m,d)::bs -> if m >= 0xc0 && m <= 0xcf && m <> 0xc4 && m <> 0xcc
-                     then (m - 0xc0, d)
-                     else find_SOF bs
+    let rec handle_marker ich c =
+      Printf.printf "Found marker ff%x\n" (int_of_char c);
+      match c with
+      | '\x01' (* TEM *)
+      | '\xD0' .. '\xD7' (* RST0-7 *)
+      | '\xD8' (* SOI *) -> find_marker ich
+      | '\xD9' (* EOI *) -> raise Not_found
+      | '\xC0' .. '\xFE' as marker (* SOF0-15, JPG, DHT, DAC *) -> (
+          let len = get_bytes ich 2 |> int_of_str2 in
+          Printf.printf "  has length %d\n" len;
+          let data = get_bytes ich (len - 2) in
+          match marker with
+          | '\xC0' .. '\xCF' ->
+            let _precision = String.sub data 0 1 |> int_of_str1 in
+            let height = String.sub data 1 2 |> int_of_str2 in
+            let width = String.sub data 3 2 |> int_of_str2 in
+            (width, height)
+          | _ -> find_marker ich
+      )
+      | _ -> find_marker ich
+    and find_marker ich =
+      match get_bytes ich 1 with
+      | "\xFF" -> find_marker ich
+      | c ->
+          handle_marker ich c.[0]
     in
-    let (n, sof) = find_SOF headers in
-    (*
-    Printf.fprintf stderr "SOF%i: " n;
-    show_string_hex sof;
-    *)
-    let height = ((int_of_char sof.[1]) lsl 8) lor (int_of_char sof.[2]) in
-    let width = ((int_of_char sof.[3]) lsl 8) lor (int_of_char sof.[4]) in
-    width, height
+    find_marker ich
 
   let parsefile fn =
     raise (Not_yet_implemented "ImageJPG.openfile") (* TODO  *)
