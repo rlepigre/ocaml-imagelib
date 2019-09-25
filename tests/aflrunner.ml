8b23eca8f3a0923398c97972a4a41094214c2d1e
Olle Jonsson <olle.jonsson@gmail.com>
2019-09-25 22:24:57 +0100

    Aflrunner: read full filenames to read files

--- tests/aflrunner.ml
+++ tests/aflrunner.ml
@@ -1,6 +1,14 @@
 let perform () =
   let chunk_reader = ImageUtil_unix.chunk_reader_of_path Sys.argv.(1) in
-  match ImageLib.PNG.ReadPNG.parsefile chunk_reader with
+
+  let extension filename =
+    let ri =
+      try 1 (* the . itself *) + String.rindex filename '.'
+      with Not_found -> invalid_arg "filename without extension"
+    in
+    String.(sub filename ri @@ (length filename) - ri)
+  in
+  match ImageLib.openfile ~extension:(extension Sys.argv.(1)) chunk_reader with
   | _ -> ()
   | exception End_of_file -> ()
   | exception Image.Corrupted_image(_) -> ()
