7e9d17b476736975ddd7964eaf7ce574e387cbd2
Olle Jonsson <olle.jonsson@gmail.com>
2019-09-25 19:11:25 +0100

    Crowbar skip known errors - and try parsefile

--- tests/tests.ml
+++ tests/tests.ml
@@ -17,20 +17,29 @@ module ImageLib_tests = struct
     let content = Crowbar.bytes in
     Crowbar.map [content] chunked_reader_with_png
 
-  let crowbar_png_size () =
-    Crowbar.add_test ~name:"ImageLib.PNG.ReadPNG.size"
-      [crowbar_gen_cr]
-      (fun cr ->
+  let crowbar_skip_known_errors f =
+    (fun cr ->
         try Crowbar.check
-        (ImageLib.PNG.ReadPNG.size cr <> (0, 0))
+        (f cr)
         with End_of_file
              | Image.Corrupted_image("Invalid PNG header...")
              | Image.Corrupted_image("Size of chunk greater than OCaml can handle...")
              | Image.Corrupted_image("Reached end of file while looking for end of chunk")
           -> Crowbar.bad_test ())
-    
+
+
+  let crowbar_png_size () =
+    Crowbar.add_test ~name:"ImageLib.PNG.ReadPNG.size"
+      [crowbar_gen_cr]
+      (crowbar_skip_known_errors(fun cr -> (ImageLib.PNG.ReadPNG.size cr <> (0, 0))))
+
+  let crowbar_png_parsefile () =
+    Crowbar.add_test ~name:"ImageLib.PNG.ReadPNG.openfile"
+      [crowbar_gen_cr]
+      (crowbar_skip_known_errors(fun cr -> (ignore @@ ImageLib.PNG.ReadPNG.parsefile cr; true)))
+
   let fuzzing : unit Alcotest.test_case list =
-    [ ("header PNG size", `Slow, crowbar_png_size) ]
+    [ ("open PNG file", `Slow, crowbar_png_parsefile) ]
 
   
 end
