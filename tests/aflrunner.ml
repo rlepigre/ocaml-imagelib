3c3a3ce31c6484df606eb95a9d5b26ca9afaeda6
Olle Jonsson <olle.jonsson@gmail.com>
2019-09-25 20:09:14 +0100

    aflrunner: Remove unused code; all Corrupted_image are the same - ignored

--- tests/aflrunner.ml
+++ tests/aflrunner.ml
@@ -1,25 +1,8 @@
-
-let crowbar_skip_known_errors f =
-(fun cr ->
-    try Crowbar.check
-    (f cr)
-    with End_of_file
-            | Image.Corrupted_image("Invalid PNG header...")
-            | Image.Corrupted_image("Size of chunk greater than OCaml can handle...")
-            | Image.Corrupted_image("Reached end of file while looking for end of chunk")
-        -> Crowbar.bad_test ())
-
-let crowbar_png_parsefile () =
-  (crowbar_skip_known_errors(fun cr -> (ignore @@ ImageLib.PNG.ReadPNG.parsefile cr; true)))
-
 let perform () =
-  let cr = ImageUtil_unix.chunk_reader_of_path Sys.argv.(1) in
-  match ImageLib.PNG.ReadPNG.parsefile cr with
+  let chunk_reader = ImageUtil_unix.chunk_reader_of_path Sys.argv.(1) in
+  match ImageLib.PNG.ReadPNG.parsefile chunk_reader with
   | _ -> ()
   | exception End_of_file -> ()
-  | exception Image.Corrupted_image("Invalid PNG header...") -> ()
-  | exception Image.Corrupted_image("Size of chunk greater than OCaml can handle...") -> ()
-  | exception Image.Corrupted_image("Reached end of file while looking for end of chunk") -> ()
-
+  | exception Image.Corrupted_image(_) -> ()
 
 let () = AflPersistent.run perform
