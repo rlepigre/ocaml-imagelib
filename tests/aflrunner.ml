c862a624d8e261f6c4121e78eda002c69c963a9b
Olle Jonsson <olle.jonsson@gmail.com>
2019-09-25 19:31:58 +0100

    Add aflrunner, a PNG-testing tool

---
+++ tests/aflrunner.ml
@@ -0,0 +1,21 @@
+
+let crowbar_skip_known_errors f =
+(fun cr ->
+    try Crowbar.check
+    (f cr)
+    with End_of_file
+            | Image.Corrupted_image("Invalid PNG header...")
+            | Image.Corrupted_image("Size of chunk greater than OCaml can handle...")
+            | Image.Corrupted_image("Reached end of file while looking for end of chunk")
+        -> Crowbar.bad_test ())
+
+let crowbar_png_parsefile () =
+  (crowbar_skip_known_errors(fun cr -> (ignore @@ ImageLib.PNG.ReadPNG.parsefile cr; true)))
+
+let () =
+  let cr = ImageUtil_unix.chunk_reader_of_path Sys.argv.(1) in
+  ignore @@ ImageLib.PNG.ReadPNG.parsefile cr;
+  ()
+
+
+
