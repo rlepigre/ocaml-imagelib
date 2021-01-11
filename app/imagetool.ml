2421d9250f7e04d80d437a5d02a705749cd887cf
Your Name <you@example.com>
2021-01-11 18:37:22 +0100

    Add --frames option for converting from animated GIFs

--- app/imagetool.ml
+++ app/imagetool.ml
@@ -1,3 +1,25 @@
+module FrameSelect = struct
+  type t = (int * int) list
+  let of_string frm : t =
+    String.split_on_char ',' frm
+    |> List.map (fun selection ->
+        begin match String.split_on_char '-' selection with
+          | [single] -> single,single
+          | [first ; last] -> first, last
+          | exception Not_found -> selection, selection
+              | _ ->
+                invalid_arg (Printf.sprintf
+                               "Unable to parse frame selection: %S" selection)
+        end |> fun (first,last) ->
+        (match first with "" -> 1 | _ -> int_of_string first),
+        (match last with "" -> max_int | _ -> int_of_string last)
+      )
+    |> List.sort compare
+  let matches t frame_number =
+    List.exists (fun (first,last) ->
+        frame_number >= first && frame_number <= last) t
+end
+
 type config = {
   input_file : string;
   display_mode: [`IRC | `VT100] option;
@@ -8,6 +30,7 @@ type config = {
   gamma: float ;
   resize : [`Original | `Percent of float | `Dim of (int * int) ];
   output_file : string option;
+  frames: FrameSelect.t; (* frames from multi-frame image, eg animated GIF *)
 }
 
 let arg_parser array : config =
@@ -17,6 +40,9 @@ let arg_parser array : config =
     function
     | [] -> config, Array.of_list leftover
     | "--help"::_ -> config, [| ""; "" |] (* return extra args *)
+    | "--frames"::frm::tl ->
+      let frames = FrameSelect.of_string frm in
+      erase leftover {config with frames} tl
     | "--background"::bg::tl ->
       erase leftover {config with background = int_of_string bg} tl
     | "--character"::fill::tl ->
@@ -63,6 +89,7 @@ let arg_parser array : config =
       gamma = 2.2;
       resize = `Original ;
       output_file = None ;
+      frames = [1, max_int];
       crop_x = 65535;
       crop_y = 65535;
     } (Array.to_list array |> List.tl)
@@ -77,6 +104,8 @@ let arg_parser array : config =
       Sys.executable_name;
     Printf.eprintf "Displays a picture in the terminal, or convert it ";
     Printf.eprintf "(if OUTPUT-FILE is specified)\n" ;
+    Printf.eprintf "The OUTPUT-FILE is a template; any '#' will be replaced with \n";
+    Printf.eprintf "the frame number (useful if the image has multiple frames)\n";
     Printf.eprintf "Options:\n";
     Printf.eprintf "--irc | --vt100   Output to terminal using escape codes\n" ;
     Printf.eprintf " \\--character     Fill character(s) for text-mode output\n";
@@ -84,9 +113,17 @@ let arg_parser array : config =
       "--resize          Set output dimensions. Syntax: '80x25' | '10%%'\n";
     Printf.eprintf "  \\--gamma        Adjust/reinterpret gamma\n";
     Printf.eprintf
-      "--crop-x WIDTH         Crop final output to WIDTH pixels\n" ;
+      "--crop-x WIDTH    Crop final output to WIDTH pixels\n" ;
     Printf.eprintf
-      "--crop-y HEIGHT        Crop final output to HEIGHT pixels\n" ;
+      "--crop-y HEIGHT   Crop final output to HEIGHT pixels\n" ;
+    Printf.eprintf
+      "--frames RANGE    Only operate on RANGE frames from a multi-frame file format\n" ;
+    Printf.eprintf
+      "                  (like an animated GIF). The format is comma-separated with '-' separating each range\n" ;
+    Printf.eprintf
+      "          y          example: --frames 1-10,20-30,50-\n" ;
+    Printf.eprintf
+      "                    example: --frames -200,300-400\n" ;
     Printf.eprintf "--background      Set background color for transparency";
     Printf.eprintf " using RGB,\n";
     Printf.eprintf "                  e.g. '0xFF0000' is red.";
@@ -132,53 +169,60 @@ let () =
     {img with width = min img.width config.crop_x ;
               height = min img.height config.crop_y }
   in
-  let foreach_img f_pre f =
-    let rec loop last_parsed_ts state =
+  let foreach_img ~frames f_pre f =
+    let rec loop frame_number last_parsed_ts state =
       match read_next state with
       | None, _, _ -> ()
       | Some o'img, delay, state ->
         let img = handle_resize o'img |> handle_crop in
         let delay = float delay /. 100. in
         let parsed_ts = Unix.gettimeofday() in
-        Unix.sleepf (max 0. (delay -. (parsed_ts -. last_parsed_ts)));
-        f_pre ();
-        foreach_pixel f img ;
-        Printf.printf "%!";
-        if state <> None then loop parsed_ts state
-    in loop 0. None
+        if FrameSelect.matches frames frame_number then begin
+          Unix.sleepf (max 0. (delay -. (parsed_ts -. last_parsed_ts)));
+          f_pre ();
+          foreach_pixel f img ;
+          Printf.printf "%!";
+        end ;
+        if state <> None then loop (succ frame_number) parsed_ts state
+    in loop 1 0. None
   in
   match config with
-  | { display_mode = Some `VT100 ; background ; character ; _ } ->
+  | { display_mode = Some `VT100 ; background ; character ; frames ; _ } ->
     (* print to terminal: using 24-bit color escape codes  *)
-    foreach_img
+    foreach_img ~frames
       (fun () -> Printf.printf "\x1b[0;0H") (* a bit annoying for debugging *)
       (ImageUtil.colorize_rgba8888 ?character ~background) ;
     print_string "\x1b[0m"
 
-  | { display_mode = Some `IRC ; background ; _ } ->
+  | { display_mode = Some `IRC ; background ; frames ; _ } ->
     (* print to terminal, using IRC 24-bit color escape codes *)
-    foreach_img (fun () -> ())
+    foreach_img ~frames (fun () -> ())
       (ImageUtil.colorize_rgba8888_irc ~background)
 
-  | { output_file = Some fn ; _ } ->
-    let img = match read_next None with
-      | Some img, _, None -> img
-      | None, _, _ -> Printf.eprintf "No frames in image." ; exit 1
-      | Some img, _, (Some _ as st) when (None, 0, None) = read_next st -> img
-      | Some _, _, Some _ ->
-        Printf.eprintf "TODO NOT IMPLEMENTED: Writing image with >1 frames" ;
+  | { output_file = Some fn ; frames ; _ } ->
+    let output_this fn img =
+      let img = handle_resize img |> handle_crop in
+      (* output to filename specified in second argument *)
+      if Sys.(file_exists fn)
+      then begin
+        Printf.eprintf "Output file %S already exists\n" fn ;
         exit 1
+      end else begin
+        let extension = extension fn in
+        let wr = ImageUtil_unix.chunk_writer_of_path fn in
+        ImageLib.writefile ~extension wr img
+      end
     in
-    let img = handle_resize img |> handle_crop in
-    (* output to filename specified in second argument *)
-    if Sys.(file_exists fn)
-    then begin
-      Printf.eprintf "Output file already exists\n" ;
-      exit 1
-    end else begin
-      let extension = extension fn in
-      let wr = ImageUtil_unix.chunk_writer_of_path fn in
-      ImageLib.writefile ~extension wr img
-    end
+    let rec loop frame_number = function
+      | Some image, _delay, state ->
+        if FrameSelect.matches frames frame_number then
+          let fn = match String.split_on_char '#' fn with
+            | exception _ -> fn
+            | parts -> String.concat (string_of_int frame_number) parts in
+          output_this fn image ;
+        loop (succ frame_number) (read_next state)
+      | None , _, _ -> exit 0
+    in
+    loop 1 (read_next None)
 
   | _ -> invalid_arg "too many argv arguments"
