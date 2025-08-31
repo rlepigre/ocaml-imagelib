ebd5b9360b9af0a157231902c0e5611a70efea81
Rodolphe Lepigre <lepigre@mpi-sws.org>
2025-08-31 10:54:30 +0200

    Improve error handing in [imagelib-convert].

--- app/convert.ml
+++ app/convert.ml
@@ -4,20 +4,20 @@ let panic : ('a,'b) koutfmt -> 'a = fun fmt ->
   let k _ = Format.eprintf "%s\n%!" (Format.flush_str_formatter ()); exit 1 in
   Format.kfprintf k Format.str_formatter fmt
 
-let handle_exception f x =
-  try f x with
-  | Image.Not_yet_implemented(msg) -> panic "Not implemented: %s." msg
-  | _                              -> panic "Unhandled exception."
-
 let _ =
   let (input_path, output_path) =
     match Sys.argv with
     | [|_;i;o|] -> (i, o)
     | _         -> panic "Usage: %s <input_file> <output_file>" Sys.argv.(0)
   in
-  let img =
+  try
     let ic = ImageUtil_unix.chunk_reader_of_path input_path in
     let extension = Filename.extension input_path in
-    handle_exception (fun ic -> ImageLib.openfile ~extension ic) ic
-  in
-  handle_exception (fun img -> ImageLib_unix.writefile output_path img) img
+    let img = ImageLib.openfile ~extension ic in
+    ImageLib_unix.writefile output_path img
+  with
+  | Image.Not_yet_implemented(msg) -> panic "Not implemented: %s." msg
+  | Image.Corrupted_image(msg)     -> panic "Corrupted image: %s." msg
+  | Sys_error(msg)                 -> panic "%s" msg
+  | e                              -> panic "Unhandled exception: %s."
+                                        (Printexc.to_string e)
